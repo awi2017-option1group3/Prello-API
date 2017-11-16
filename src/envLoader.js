7ffd2cfd39480101cf6f0a87780bfbb2c0dfe315
Théo Gauchoux <theo.gauchoux.pro@gmail.com>
2017-11-16 18:27:59 +0100

    Remove .env for production

--- src/envLoader.js
+++ src/envLoader.js
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv'
 import path from 'path'
 
-dotenv.config({ path: path.join(path.dirname(__dirname), '/.env') })
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: path.join(path.dirname(__dirname), '/.env') })
+}
